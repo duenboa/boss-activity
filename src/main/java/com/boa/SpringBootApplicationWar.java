88999b0c582cfa719c79e36b9736766f0c09d1c0
duenboa <duenboa@163.com>
2017-11-06 22:06:22 +0800

    compiler to war package. NOTICE: tomcat version 8. rename .war file to ROOT.war and put into TOMCAT_HOME/webapps/ folder.

--- src/main/java/com/boa/SpringBootApplicationWar.java
+++ src/main/java/com/boa/SpringBootApplicationWar.java
@@ -6,7 +6,9 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * 这种方式来构建war包. 独立到tomcat中发布
+ * <p>
  * <b>修改点:</b>
+ * <p>
  * 1. pom文件中的打包方式: 指定为war
  * <pre>
  *     <groupId>com.boa</groupId>
@@ -14,21 +16,22 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
  *     <version>0.0.1-SNAPSHOT</version>
  *     <!--<packaging>jar</packaging>-->
  *     <packaging>war</packaging>
+ *</pre>
  *
- *
+ * <pre>
  * 2. 配置启动类
- * <code>
+ *     #@Controller
+ *     #@EnableAutoConfiguration
+ *     #@SpringBootApplication
+ *     public class SpringBootApplicationWar extends SpringBootServletInitializer {...}
  *
- * @Controller
- * @EnableAutoConfiguration
- * @SpringBootApplication
- * public class SpringBootApplicationWar extends SpringBootServletInitializer {
- * </code>
- *
+ * 3. 将maven中的依赖: spring-boot-starter-tomcat 的scope设置为 provide类型. 编译时提供,打包时排除.
+ *      <dependency>
+ *      <groupId>org.springframework.boot</groupId>
+ *      <artifactId>spring-boot-starter-tomcat</artifactId>
+ *      <scope>provided</scope>
+ *      </dependency>
  * </pre>
- * <p>
- * 3.
- * 将maven中的依赖: spring-boot-starter-tomcat 的scope设置为 provide类型. 编译时提供,打包时排除.
  */
 public class SpringBootApplicationWar extends SpringBootServletInitializer {
 
